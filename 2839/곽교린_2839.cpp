3b247c57fe4fa8553943785108c184dabd31ed88
kwakkyoleen <52497663+kwakkyoleen@users.noreply.github.com>
2023-04-05 11:26:19 +0900

    Update 곽교린_2839.cpp

--- 2839/곽교린_2839.cpp
+++ 2839/곽교린_2839.cpp
@@ -10,7 +10,7 @@ int main()
     int num_5 = weight / 5; //일단 5키로 봉투로 꽉 채우고
     int num_3 = (weight - num_5*5)/3; //남은 무게를 3키로로 채움
     while (num_5 >= 0) {
-        if (num_5 * 5 + num_3 * 3 == weight) { //만약 설탕봉지들의 무게가 총 설탕 무게보다 작으면 5개짜리 빼고 3개짜리 2개넣음
+        if (num_5 * 5 + num_3 * 3 == weight) { //만약 설탕봉지들의 무게가 총 설탕 무게보다 작으면 5키로짜리 하나 빼고 3키로짜리 2개넣음
             cout << num_5 + num_3;
             break;
         }
