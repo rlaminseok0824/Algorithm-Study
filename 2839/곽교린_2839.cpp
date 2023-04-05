4e4d6827042338557dabebd501435db8da60cdb2
kwakkyoleen <2007kkl@naver.com>
2023-04-05 10:56:03 +0900

    Create 곽교린_2839.cpp

---
+++ 2839/곽교린_2839.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int weight;
+    cin >> weight;
+
+    int num_5 = weight / 5; //일단 5키로 봉투로 꽉 채우고
+    int num_3 = (weight - num_5*5)/3; //남은 무게를 3키로로 채움
+    while (num_5 >= 0) {
+        if (num_5 * 5 + num_3 * 3 == weight) { //만약 설탕봉지들의 무게가 총 설탕 무게보다 작으면 5개짜리 빼고 3개짜리 2개넣음
+            cout << num_5 + num_3;
+            break;
+        }
+        else if (num_5 * 5 + num_3 * 3 < weight) {
+            num_5--;
+            num_3 += 2;
+        }
+    }
+    if (num_5 < 0) { //안되면 -1
+        cout << -1;
+    }
+
+
+    return 0;
+}
