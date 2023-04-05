9d219a644f1dd11e22bcc9c8ba7bdb35867530ea
kwakkyoleen <2007kkl@naver.com>
2023-04-05 19:29:03 +0900

    Create 곽교린_11722.cpp

---
+++ 11722/곽교린_11722.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <memory.h>
+
+using namespace std;
+
+int main()
+{
+    int num = 0, maxnum = 0;
+    cin >> num;
+    int numlist[1001];
+    int dp[1001];
+    memset(numlist, 0, sizeof(numlist));
+    memset(dp, 0, sizeof(dp));
+    for (int i = 0; i < num; i++) {
+        cin >> numlist[i];
+        dp[i] = 1;
+    }
+    for (int i = 1; i < num; i++) {
+        for (int j = 0; j < i; j++) {
+            if (numlist[j] > numlist[i] && dp[j] >= dp[i]) {
+                dp[i] = dp[j] + 1;
+            }
+        }
+    }
+    for (int i = 0; i < num; i++) {
+        if (dp[i] > maxnum)
+            maxnum = dp[i];
+    }
+    cout << maxnum;
+}
