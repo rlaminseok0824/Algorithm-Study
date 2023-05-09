d1a1cee1f34b51a8a76731735a75c6d924f94cc7
kkddhh7 <128781148+kkddhh7@users.noreply.github.com>
2023-05-09 16:10:01 +0900

    Update 김도현_12851.cpp

--- 12851/김도현_12851.cpp
+++ 12851/김도현_12851.cpp
@@ -3,92 +3,77 @@
 #include <queue>
 using namespace std;
 
-class HS {
-	int slocation;
-	int blocation;
-	int time;
-	int count;
-	queue<int> qq;
-	int array[100001];
-public:
-	void get_location();
-	void branch();
-	vector<int> make_number(int a);
-	void start();
-};
 
-void HS::get_location() {
-	int n, m;
-	cin >> n >> m;
-	slocation = n;
-	blocation = m;
-	qq.push(m);
-	array[m - 1] = 1;
-}
-
-vector<int> HS::make_number(int a) {
-	int temp;
-	vector<int> numbers;
-
-	if (a % 2 == 0) {
-		temp = a / 2;
-		if (slocation / 2 - 1 < temp && blocation + 2 > temp) {
-			if (array[temp - 1] >= time || array[temp - 1] == 0) {
-				numbers.push_back(temp);
-				array[temp - 1] = time;
-			}
-		}
-	}
-
-	temp = a - 1;
-	if (slocation / 2 - 1 < temp && blocation + 2 > temp) {
-		if (array[temp - 1] >= time || array[temp - 1] == 0) {
-			numbers.push_back(temp);
-			array[temp - 1] = time;
-		}
-	}
-
-	temp = a + 1;
-	if (slocation / 2 - 1 < temp && blocation + 2 > temp) {
-		if (array[temp - 1] >= time || array[temp - 1] == 0) {
-			numbers.push_back(temp);
-			array[temp - 1] = time;
-		}
-	}
-
-	return numbers;
-}
-
-void HS::branch() {
-	while (count == 0) {
-		time += 1;
-		int tcount = 0;
-		int size = qq.size();
-		for (int i = 0; i < size; i++) {
-			int a = qq.front();
-			qq.pop();
-			vector<int> ttemp = make_number(a);
-			for (int j = 0; j < ttemp.size(); j++) {
-				if (ttemp[j] == slocation) {
-					tcount += 1;
-				}
-				else {
-					qq.push(ttemp[j]);
-				}
-			}
-		}
-		count = tcount;
-	}
-}
-
-void HS::start() {
-	get_location();
-	branch();
-
-	cout << time << endl << count;
-}
 
 int main() {
-	HS hide_and_seek;
-	hide_and_seek.start();
+	int slocation;
+	int blocation;
+	int time = 0;
+	int count = 0;
+	int tcount = 0;
+	queue<int> qq;
+	int array[100002] = { 0, };
+
+
+	cin >> slocation >> blocation;
+	qq.push(blocation);
+	array[blocation] = 1;
+
+	if (slocation >= blocation) {
+		int num = slocation - blocation;
+		cout << num << endl << 1;
+	}
+	else {
+		while (count == 0) {
+			time += 1;
+			int size = qq.size();
+			for (int i = 0; i < size; i++) {
+				int a = qq.front();
+				qq.pop();
+				int temp;
+				if (a % 2 == 0) {
+					temp = a / 2;
+					if (slocation / 2 <= temp && blocation + 2 > temp && 0 <= temp && temp <= 100000) {
+						if (array[temp] >= time || array[temp] == 0) {
+							array[temp] = time;
+							if (temp == slocation) {
+								tcount += 1;
+							}
+							else {
+								qq.push(temp);
+							}
+						}
+					}
+				}
+
+				temp = a - 1;
+				if (slocation / 2 <= temp && blocation + 2 > temp && 0 <= temp && temp <= 100000) {
+					if (array[temp] >= time || array[temp] == 0) {
+						array[temp] = time;
+						if (temp == slocation) {
+							tcount += 1;
+						}
+						else {
+							qq.push(temp);
+						}
+					}
+				}
+
+				temp = a + 1;
+				if (slocation / 2 <= temp && blocation + 2 > temp && 0 <= temp && temp <= 100000) {
+					if (array[temp] >= time || array[temp] == 0) {
+						array[temp] = time;
+						if (temp == slocation) {
+							tcount += 1;
+						}
+						else {
+							qq.push(temp);
+						}
+					}
+				}
+			}
+			count = tcount;
+		}
+		cout << time << endl << count;
+	}
 }
